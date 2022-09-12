f6c65ead3691fa539513eb121c3b697334e5ec3b
t0933095470 <thethao165@gmail.com>
2022-09-12 09:39:08 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Do - While loop 
+// Write "Hello World" 5 times
+int count = 1;
+do //Do not check condition on first iteration
+{
+    Console.WriteLine("Hello World");
+    count++;
+} while (count <= 5); //check condition: if true, do block execute
